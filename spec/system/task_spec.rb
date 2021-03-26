ccdf849d514dc75d27b17b48dee96b9ba35fa69f
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-26 09:06:19 +0000

    taskを作成日の降順にする機能のSystemSpec作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,22 +1,20 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task03) { FactoryBot.create(:task, title: 'task03', content: '内容03', created_at: '2021-03-26 17:03') }
+  let!(:task01) { FactoryBot.create(:task, title: 'task01', content: '内容01', created_at: '2021-03-26 17:01') }
+  let!(:task02) { FactoryBot.create(:task, title: 'task02', content: '内容02', created_at: '2021-03-26 17:02') }
+
+  before do
+    visit tasks_path
+  end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-        # 1. new_task_pathに遷移する（新規作成ページに遷移する）
         visit new_task_path
-        # 2. 新規登録内容を入力する
-        #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
-        # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in "task_title", with: 'タスク名'
-        # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
         fill_in "task_content", with: 'タスク詳細'
-        # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
-        # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
         click_button 'commit'
-        # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-        # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-        # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
         expect(page).to have_content 'タスク名'
         expect(page).to have_content 'タスク詳細'
       end
@@ -25,33 +23,32 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
-        # タスク一覧ページに遷移
-        visit tasks_path
         # ページ遷移直後でデバッグ
         # binding.irb
-        # タスク一覧ページに遷移できているかを確認
+        # タスク一覧ページに遷移できているかをデバッグ
         current_path
-        # タスクがデータベースに作成されているかを確認
+        # タスクがデータベースに作成されているかをデバッグ
         Task.count
-        # 表示するHTMLにタスク情報が入っているかを確認
+        # 表示するHTMLにタスク情報が入っているかをデバッグ
         page.html
-        expect(page).to have_content 'task'
+        expect(page).to have_content 'task01'
+      end
+    end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        task_list = all('.task_row')
+        expect(task_list[0]).to have_content '2021-03-26 17:01'
+        expect(task_list[1]).to have_content '2021-03-26 17:02'
+        expect(task_list[2]).to have_content '2021-03-26 17:03'
       end
     end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-        #タスク1個作成
-        FactoryBot.create(:task)
-        #一覧画面を表示
-        visit tasks_path
-        #'taskの詳細'をクリック
-        click_link 'taskの詳細'
+        click_link 'taskの詳細', match: :first
         #登録タスクの内容が表示
-        expect(page).to have_content 'task'   
+        expect(page).to have_content 'task01'   
        end
      end
   end
