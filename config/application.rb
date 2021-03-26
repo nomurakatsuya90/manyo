074f3cb3786354147c72431a3df6e8d9aac3732e
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-26 07:52:34 +0000

    JSTタイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Manyo
                        controller_specs: false,
                        request_specs: false
     end
+    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
