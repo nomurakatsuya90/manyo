564e3685912d79fa8762b50042686d03afa5d19b
katsuya_nomura <panta_rhei@suf.view21.net>
2021-03-31 04:16:03 +0000

    終了期限・ステータス・優先順位の登録機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,7 +49,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:title, :content)
+    params.require(:task).permit(:title, :content, :expired_at, :status, :priority)
   end
 
   def set_task
