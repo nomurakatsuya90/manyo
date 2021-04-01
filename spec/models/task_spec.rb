ce1dfc6a259973230f7b360db32c37bfb399d28d
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-01 09:10:15 +0000

    ModelSpec(タイトルのあいまい検索) OK

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -17,8 +17,30 @@ describe 'タスクモデル機能', type: :model do
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
         # ここに内容を記載する
-        task = Task.new(title: '成功テスト', content: '成功テスト')
+        task = Task.new(title: '成功テスト', content: '成功テスト',expired_at: '2022-04-29　16:28', status: '完了', priority: '高')
         expect(task).to be_valid
+      end
+    end
+  end
+  describe '検索機能' do
+    let!(:task01) { FactoryBot.create(:task, title: 'task01', status: '完了') }
+    let!(:task02) { FactoryBot.create(:task, title: "task02", status: '着手中') }
+    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
+      it "検索キーワードを含むタスクが絞り込まれる" do
+        # title_seachはscopeで提示したタイトル検索用メソッドである。メソッド名は任意で構わない。
+        expect(Task.title_search('k01')).to include(task01)
+        expect(Task.title_search('k01')).not_to include(task02)
+        expect(Task.title_search('k01').count).to eq 1
+      end
+    end
+    context 'scopeメソッドでステータス検索をした場合' do
+      it "ステータスに完全一致するタスクが絞り込まれる" do
+        # expect(Task.status_search('完了').status).to eq(task01)
+      end
+    end
+    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
+      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        # ここに内容を記載する
       end
     end
   end
