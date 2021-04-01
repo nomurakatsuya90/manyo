5a2a12da4be1bcb40de8ecbeef1224016c67c8e6
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-01 08:01:33 +0000

    SystemSpec(ステータス検索) OK

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
     content { 'test_content' }
     created_at { '2021-03-26 17:00' }
     expired_at { '2021-04-01 10:26' }
+    status { '未着手' }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
-  let!(:task03) { FactoryBot.create(:task, title: 'task03', content: '内容03', created_at: '2021-03-26 17:03', expired_at: '2021-04-03 16:01') }
-  let!(:task01) { FactoryBot.create(:task, title: 'task01', content: '内容01', created_at: '2021-03-26 17:01', expired_at: '2021-04-01 17:02') }
-  let!(:task02) { FactoryBot.create(:task, title: 'task02', content: '内容02', created_at: '2021-03-26 17:02', expired_at: '2021-04-02 15:02') }
+  let!(:task03) { FactoryBot.create(:task, title: 'task03', content: '内容03', created_at: '2021-03-26 17:03', expired_at: '2021-04-03 16:01', status: '未着手') }
+  let!(:task01) { FactoryBot.create(:task, title: 'task01', content: '内容01', created_at: '2021-03-26 17:01', expired_at: '2021-04-01 17:02', status: '着手中') }
+  let!(:task02) { FactoryBot.create(:task, title: 'task02', content: '内容02', created_at: '2021-03-26 17:02', expired_at: '2021-04-02 15:02', status: '完了') }
 
   before do
     visit tasks_path
@@ -49,6 +49,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '「終了期限でソート」を押下した場合' do
       it '終了期限が早い順番で上から表示される' do
         click_link '終了期限でソート'
+        sleep(0.5)
         task_list = all('.task_expired_at')
         expect(task_list[0]).to have_content '2021-04-01　17:02'
         expect(task_list[1]).to have_content '2021-04-02　15:02'
@@ -67,8 +68,13 @@ RSpec.describe 'タスク管理機能', type: :system do
           end
           context 'ステータス検索をした場合' do
             it "ステータスに完全一致するタスクが絞り込まれる" do
-              # ここに実装する
-              # プルダウンを選択する「select」について調べてみること
+              select '完了', from: 'status'
+              click_on 'commit'
+              sleep(0.5)
+              #完了/未着手/着手中の文字列は検索用にあるため期待値に用いない
+              expect(page).to have_content 'task02'      #完了のタスク
+              expect(page).not_to have_content 'task01'  #着手中のタスク
+              expect(page).not_to have_content 'task03'  #未着手のタスク
             end
           end
           context 'タイトルのあいまい検索とステータス検索をした場合' do
