6140f797bf7673f4ccebe33aa3487032e4086d9f
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-04 10:19:47 +0000

    管理者画面作成。最初の管理者をseedで登録

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,13 +1,56 @@
 class Admin::UsersController < ApplicationController
+  before_action :admin_user
+  before_action :set_user, only: [:show, :edit, :destroy, :update]
+
   def index
+    @users = User.all.includes(:tasks)
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザ「#{@user.name}」を登録しました"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to admin_users_path, notice: '#{@user.name}さんのプロフィールを編集しました'
+    else
+      render :edit
+    end
+  end
+
   def show
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to admin_users_path, notice: 'ユーザを削除しました'
+    else
+      redirect_to admin_users_path, notice: '本ユーザを削除すると管理者が不在となり削除できません。'
+    end
+  end
+
+  private
+  def admin_user
+    redirect_to root_path, notice: '管理者以外はアクセスできません' unless current_user.admin?
+  end
+
+  def set_user
+    @user = User.includes(:tasks).find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:id, :name, :email, :admin, :password, :password_confirmation)
   end
 end
