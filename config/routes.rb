835b592a3d99d9afcdf900f297647ab16024ef28
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-04 03:25:04 +0000

    adminとsessionのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  resources :tasks do
-    # collection do
-    #   post :confirm
-    # end
+  namespace :admin do
+    resources :users
   end
+  resources :tasks
   root to: 'tasks#index'
-  # get 'tasks/index'
-  # get 'tasks/new'
-  # get 'tasks/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
