b95a78ab051d7e238dd780f08e9b756fbc732db4
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-04 02:09:26 +0000

    DBマイグレーション users追加、index、外部キー追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_013413) do
+ActiveRecord::Schema.define(version: 2021_04_04_015037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,19 @@ ActiveRecord::Schema.define(version: 2021_03_31_013413) do
     t.datetime "expired_at", default: -> { "now()" }, null: false
     t.integer "priority", default: 0, null: false
     t.integer "status", default: 0, null: false
+    t.bigint "user_id"
     t.index ["status"], name: "index_tasks_on_status"
     t.index ["title"], name: "index_tasks_on_title"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "tasks", "users"
 end
