1039beaf1608eeef9e41bfc64c26e7b01d37434a
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-04 08:13:15 +0000

    自分が作成したタスクだけを表示。登録後即ログイン(showのcurrent_user判定をパスするように)。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # session[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id), notice: "登録しました"
     else
       render :new
