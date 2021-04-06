e424f2340e214e647a786d356356befe661d7afc
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-06 07:32:57 +0000

    SystemSpec(管理者画面：管理者はアクセス可/一般ユーザーはアクセス不可) OK

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -17,22 +17,22 @@ RSpec.describe 'ログイン機能', type: :system do
     end
 
     context 'ログインせずにタスク一覧画面へアクセスした場合' do
-        it 'ログイン画面に遷移する' do
-          visit tasks_path
-          expect(page).to have_content 'Log in'
-        end
+      it 'ログイン画面に遷移する' do
+        visit tasks_path
+        expect(page).to have_content 'Log in'
+      end
     end
   end
 
   describe 'セッション機能' do
     context 'ユーザーが登録されている場合' do
-        it 'ログインができる' do
-          visit new_session_path
-          fill_in 'session_email', with: 'user01@example.jp'
-          fill_in 'session_password', with: 'pass1234'
-          click_on 'commit'
-          expect(page).to have_content '一般ユーザー01のページ'
-        end
+      it 'ログインができる' do
+        visit new_session_path
+        fill_in 'session_email', with: 'user01@example.jp'
+        fill_in 'session_password', with: 'pass1234'
+        click_on 'commit'
+        expect(page).to have_content '一般ユーザー01のページ'
+      end
     end
     context 'ログインしている場合' do
       before do
@@ -41,18 +41,75 @@ RSpec.describe 'ログイン機能', type: :system do
         fill_in 'session_password', with: 'pass1234'
         click_on 'commit'
       end
-        it '自分のプロフィール画面に遷移' do
-          visit user_path(user01.id)
-          expect(page).to have_content '一般ユーザー01のページ'
-        end
-        it '他人の詳細画面にアクセスするとタスク一覧画面に遷移' do
-          visit user_path(admin01.id)
-          expect(page).to have_content 'タスク一覧'
-        end
-        it 'ログアウトができる' do
-          click_link 'Logout'
-          expect(page).to have_content 'Log in'
-        end
+      it '自分のプロフィール画面に遷移' do
+        visit user_path(user01.id)
+        expect(page).to have_content '一般ユーザー01のページ'
+      end
+      it '他人の詳細画面にアクセスするとタスク一覧画面に遷移' do
+        visit user_path(admin01.id)
+        expect(page).to have_content 'タスク一覧'
+      end
+      it 'ログアウトができる' do
+        click_link 'Logout'
+        expect(page).to have_content 'Log in'
+      end
+    end
+  end
+
+  describe '管理者画面の機能' do
+    context '一般ユーザーの場合' do
+      it '管理者画面へアクセスするとタスク一覧画面に遷移' do
+        visit new_session_path
+        fill_in 'session_email', with: 'user01@example.jp'
+        fill_in 'session_password', with: 'pass1234'
+        click_on 'commit'
+        sleep(0.5)
+        visit admin_users_path
+        expect(page).to have_content 'タスク一覧'
+      end
+    end
+    context '管理ユーザーの場合' do
+      before do
+        visit new_session_path
+        fill_in 'session_email', with: 'admin01@example.jp'
+        fill_in 'session_password', with: 'pass1234'
+        click_on 'commit'
+      end
+      it '管理者画面にアクセスできる' do
+        sleep(0.5)
+        click_link '管理者画面へ'
+        sleep(0.5)
+        expect(page).to have_content '管理者画面'
+      end
+    #   it 'ユーザーの新規登録ができる' do
+    #     click_link '管理者画面へ' 
+    #     click_link 'ユーザーの新規登録'
+    #     fill_in 'Name', with: 'test'
+    #     fill_in 'Email', with: 'test@example.com'
+    #     fill_in 'Password', with: '123456'
+    #     fill_in 'Password confirmation', with: '123456'
+    #     click_on 'ユーザーの登録'
+    #     expect(page).to have_content 'ユーザーを登録しました'     
+    #   end
+    #   it 'ユーザーの詳細画面へアクセスできる' do
+    #     visit admin_user_path(user.id)
+    #     expect(page).to have_content '一般ユーザーのページ'
+    #   end
+    #   it 'ユーザの編集画面でユーザを編集できる' do
+    #     visit admin_users_path
+    #     all('tbody tr')[0].click_on 'ユーザの編集'
+    #     fill_in 'Name', with: 'edit_test'
+    #     fill_in 'Email', with: 'edit_test@example.com'
+    #     choose 'user_admin_true'
+    #     click_on '変更する'
+    #     expect(page).to have_content 'ユーザーを編集しました'
+    #   end
+    #   it 'ユーザーの削除ができる' do
+    #     visit admin_users_path
+    #     all('tbody tr')[0].click_on 'ユーザの削除'
+    #     page.driver.browser.switch_to.alert.accept
+    #     expect(page).to have_content 'ユーザーを削除しました'
+    #   end
     end
   end
 end
