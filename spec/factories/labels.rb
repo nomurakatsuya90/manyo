de842318e053f1c0da9e976230f6670ce76e2932
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-12 04:33:12 +0000

    SystemSpec(ラベル新規作成) OK

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
-  factory :label do
-    name { "MyString" }
+  factory :label01, class: Label do
+    name { "LABEL01" }
+  end
+end
+
+FactoryBot.define do
+  factory :label02, class: Label do
+    name { "LABEL02" }
   end
 end
