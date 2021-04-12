40a040dd356e0c07c9fdc1b3e4fa4501f000bce6
katsuya_nomura <panta_rhei@suf.view21.net>
2021-04-12 04:45:54 +0000

    SystemSpec(詳細画面ラベル一覧) OK

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -26,6 +26,7 @@ RSpec.describe 'ラベル登録機能', type: :system do
         FactoryBot.create(:label01)
         FactoryBot.create(:label02)
         visit new_task_path
+        sleep(0.5)
         fill_in 'task_title', with: 'タスク名'
         fill_in 'task_content', with: 'タスク詳細'
         fill_in :task_expired_at, with: '2021-04-01 10:35'.to_datetime
@@ -43,25 +44,26 @@ RSpec.describe 'ラベル登録機能', type: :system do
     end
   end
 
-  # describe '詳細確認機能' do
-  #   context '詳細画面に遷移した場合' do
-  #     it 'ラベルが表示される' do
-  #       user = FactoryBot.create(:task_user2)
-  #       login(user)
-  #       label = FactoryBot.create(:label01)
-  #       visit new_task_path
-  #       fill_in 'Title', with: 'aaa'
-  #       fill_in 'Details', with: 'bbb'
-  #       fill_in :task_expired_at, with: '002021-03-17　11:39'
-  #       select '未着手', from: 'status'
-  #       select '中', from: 'priority'
-  #       check 'Task1'  
-  #       click_on 'Create Task' 
-  #       click_on '詳細画面'
-  #       expect(page).to have_content 'Task1'
-  #     end
-  #   end
-  # end
+  describe '詳細確認機能' do
+    context '詳細画面に遷移した場合' do
+      it 'ラベルが表示される' do
+        user = FactoryBot.create(:user01)
+        login(user)
+        label = FactoryBot.create(:label01)
+        visit new_task_path
+        sleep(0.5)
+        fill_in 'task_title', with: 'タスク名'
+        fill_in 'task_content', with: 'タスク詳細'
+        fill_in :task_expired_at, with: '2021-04-01 10:35'.to_datetime
+        select '着手中', from: 'status'
+        select '中', from: 'priority'
+        check 'LABEL01'
+        click_button 'commit'
+        sleep(0.5)
+        expect(page).to have_content 'LABEL01'
+      end
+    end
+  end
 
   # describe '検索機能' do
   #   context 'ラベルで検索した場合' do
